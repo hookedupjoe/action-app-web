1112c3ed8689632ef66246d59fc8f890d842a209
Joseph Francis <joefran@us.ibm.com>
2019-08-21 15:40:31 -0500

    Added controls page

--- www/app/app.js
+++ www/app/app.js
@@ -8,6 +8,7 @@
     'PromptsPage', 
     'JsonHelperPage', 
     'UsingControlsPage', 
+    'ControlsPage', 
     'LogsPage'
   ];
 
@@ -27,11 +28,18 @@
       //--- Items to load when the application loads
       var tmpRequired = {}
 
+      var appIndex = {
+        res: {
+          panels: 'catalog/panels/common'
+        }
+      }
+
       ThisApp.init({ pages: thePages, plugins: thePlugins, required: tmpRequired }).then(function (theReply) {
         ThisApp.getByAttr$({ appuse: "app-loader" }).remove();
 
         //--- Extend common with your app specific stuff
         $.extend(ThisApp.common, {
+          index: appIndex,
           samplesBaseURL: 'catalog/panels/samples'
         })
 
