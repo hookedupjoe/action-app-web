67909cd1cb054cc2d44bfc6788d5478d10b62cc8
Joseph Francis <hookedupjoe@gmail.com>
2020-12-16 14:09:05 -0500

    app only init sync

--- www/lib/actionapp/app-only-init.js
+++ www/lib/actionapp/app-only-init.js
@@ -3,7 +3,7 @@
 
   var tmpBody = $('body');     
   if( tmpBody.length == 0 ){
-      console.log('App Only Init - No Body Found - Aborting Init');
+      //console.log('App Only Init - No Body Found - Aborting Init');
       return;
   } 
   var tmpPluginNames = [];
@@ -11,6 +11,7 @@
 
   ActionAppCore = ActionAppCore || window.ActionAppCore;
 
+  
   function setup(thePages, thePlugins, theUseLayout) {
     try {
       var siteMod = ActionAppCore.module('site');
@@ -21,20 +22,25 @@
 
       var tmpSetAlert = true;
       var tmpAlertEl = $('body');
-      //--- See if we have full semantic loaded with modal function
-      //   if not, do not replace the standard alert
-      console.log("typeof(tmpAlertEl.modal)",typeof(tmpAlertEl.modal))
       if( typeof(tmpAlertEl.modal) != 'function'){
-        console.log("no set")
         tmpSetAlert = false;
       }
 
       //--- Items to load when the application loads
       var tmpRequired = {}
-      var tmp
+      ActionAppCore.subscribe('app-loaded', function(){
+        ThisApp.initAppComponents();
+      });
       //--- Use tmpRequiredSpecs to preload more using that example
       ThisApp.init({ setAlert: tmpSetAlert, layout: false, pages: thePages, plugins: thePlugins, required: tmpRequired }).then(function (theReply) {
         ThisApp.getByAttr$({ appuse: "app-loader" }).remove();
+
+        
+        window.addEventListener('resize', ThisApp.grid16.onResize );
+
+
+        ThisApp.grid16.resizeLayoutProcess();
+        
 
         //--- Extend common with your app specific stuff
         $.extend(ThisApp.common, {
